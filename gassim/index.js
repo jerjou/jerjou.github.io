ff0f7e04b4ef7fbac2f5f4e37fff32f8cb8bd698
jerjou <jerjou@users.noreply.github.com>
2020-08-05 12:41:10 -0400

    Add snapshot of gassim pages

---
+++ gassim/index.js
@@ -0,0 +1,235 @@
+import init, { World } from './js/gassim.js';
+
+const $ = q => document.querySelector(q);
+const $el = (t, p) => {
+  const el = document.createElement(t);
+  if (p) for (const [k,v] of Object.entries(p)) el[k] = v;
+  return el;
+};
+const pint = v => parseInt(v, 10);
+const pfloat = v => parseFloat(v, 10);
+
+// Returns coordinates relative to the canvas
+function coord(e) {
+  const rect = e.target.getBoundingClientRect();
+  const y = rect.height - (e.clientY - rect.top);
+  const left = e.clientX - rect.left;
+  return [left, y];
+}
+
+let control = {
+  init: (wasm, canvas) => {
+    let play = $('#play');
+    play.addEventListener('change', (e) => {
+      if (play.checked) {
+        control.renderLoop();
+      }
+    });
+    Object.assign(control, {
+      playEl: play,
+      ctx: canvas.getContext('2d'),
+      wasm: wasm,
+    });
+
+    control.initMouse(canvas);
+    control.initKeyboard();
+    control.initHeat(canvas);
+    control.initParticles(canvas);
+    control.initNumberScroll();
+    control.initWorld(canvas);
+  },
+
+  initWorld: canvas => {
+    const TIMESTEP_FACTOR = 2000;
+    let form = $('form.tab.setup');
+    let resetWorld = _ => {
+      let width = pint(form.width.value);
+      let height = pint(form.height.value);
+      let numPoints = pint(form.n.value);
+      let radius = pint(form.radius.value);
+      let mass = pfloat(form.density.value) * radius * radius * 3.14159;
+      let heat = pint(form.heat.value);
+      let gravity = pfloat(form.gravity.value) * 100;
+      let vdw = pint(form.vdw.value) * 1000;
+
+      canvas.style.width = `${canvas.width = width}px`;
+      canvas.style.height = `${canvas.height = height}px`;
+
+      control.world = World.new(width, height, gravity, numPoints, radius, mass, heat, vdw);
+      form.timestep.value = Math.round(control.world.timestep() * TIMESTEP_FACTOR);
+    };
+
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      resetWorld();
+    });
+    resetWorld();
+
+    // Ability to update some properties on the fly
+    form.timestep.addEventListener('change', e => {
+      control.world.set_timestep(pint(e.target.value, 1) / TIMESTEP_FACTOR);
+    });
+    form.gravity.addEventListener('change', e => {
+      control.world.set_gravity(pfloat(e.target.value, -9.8) * 100);
+    });
+
+    control.renderLoop();
+  },
+
+  // Use the scroll wheel to change the number like a dial
+  initNumberScroll: () => {
+    document.body.addEventListener('wheel', e => {
+      let el = e.target;
+      if (!el.matches('input[type=number]')) return;
+      if (el.step === 'any') return;
+      let numIters = ~~Math.abs(Math.round(e.deltaY));
+      if (e.deltaY < 0) {
+        for (let i = 0; i< numIters; i++) el.stepUp();
+      } else {
+        for (let i = 0; i< numIters; i++) el.stepDown();
+      }
+    });
+  },
+
+  initMouse: canvas => {
+    window.addEventListener('mouseup', e => control.mousedown = false);
+    canvas.addEventListener('mousemove', e => control.mouseCoord = coord(e));
+    canvas.addEventListener('mouseenter', e => control.mouseCoord = coord(e));
+  },
+
+  // keyboard shortcuts
+  kbdShortcuts: {
+    f: e => $('#heatTab').checked = true,
+    h: e => $('#setupTab').checked = true,
+    o: e => $('#addTab').checked = true,
+    p: e => {
+      let p = $('#play');
+      p.checked = !p.checked;
+      p.dispatchEvent(new InputEvent('change'));
+    },
+  },
+  initKeyboard: _ => {
+    window.addEventListener('keydown', e => {
+      (control.kbdShortcuts[e.key] || console.log)(e);
+    });
+  },
+
+  initHeat: canvas => {
+    let heatTab = $('#heatTab');
+    canvas.addEventListener('mouseenter', e => {
+      if (pfloat($('form.tab.heat').factor.value) < 1) {
+        canvas.classList.add('cold');
+      } else {
+        canvas.classList.remove('cold');
+      }
+    });
+    canvas.addEventListener('mousedown', e => {
+      if (heatTab.checked) control.addHeat(e);
+    });
+  },
+
+  // Adding/remove particles as you drag around the canvas
+  initParticles: canvas => {
+    canvas.addEventListener('mousedown', (e) => {
+      if ($('#addTab').checked) control.addParticles(e);
+    });
+  },
+
+  // `rate` is in particles/60 frames
+  _continuouslyDo: (rate, f, threshold=30, iter=0) => {
+    control.mousedown = true;
+    let remainder = 1;
+    let numParticles = _ => {
+      let thistime = (rate / 60) + remainder;
+      remainder = thistime % 1;
+      return thistime - remainder;
+    };
+
+    function doF(e) {
+      let n = numParticles(iter++);
+      if (n) {
+        f(...control.mouseCoord, n, iter);
+      }
+      if (control.mousedown) requestAnimationFrame(doF);
+    }
+    doF();
+  },
+
+  addParticles: e => {
+    let form = $('form.tab.add');
+    let radius = pint(form.radius.value);
+    let [rate, mass, heat] = [
+      pint(form.rate.value),
+      pfloat(form.density.value) * radius * radius * 3.14159,
+      pint(form.heat.value)];
+    let add = form.modifyParticles.value === 'add';
+
+    if (add) {
+      control._continuouslyDo(rate, (x, y, numParticles) => {
+        control.world.add_particles(x, y, radius, mass, heat, numParticles);
+        // Update the world so we can see the new particle
+        if (!control.playEl.checked) {
+          control.renderLoop(0, control.world.update_positions());
+        }
+      });
+    } else {
+      control._continuouslyDo(rate, (x, y) => {
+        control.world.remove_particles(x, y, 128);
+        // Update the world so we can see the new particle
+        if (!control.playEl.checked) {
+          control.renderLoop(0, control.world.update_positions());
+        }
+      });
+    }
+  },
+
+  addHeat: e => {
+    let form = $('form.tab.heat');
+    let [factor, radius] = [pfloat(form.factor.value) / 5, pint(form.radius.value)];
+
+    control._continuouslyDo(30, (x, y) => control.world.temperature(x, y, radius, factor));
+  },
+
+  renderLoop: (time, positionsPtr) => {
+    const tuple_len = 3;
+    if (!positionsPtr) {
+      positionsPtr = control.world.step();
+    }
+    const numPoints = control.world.num_particles;
+    const coords = new Float32Array(
+      control.wasm.memory.buffer, positionsPtr, numPoints * tuple_len)
+
+    //control.world.temperature(.99);
+
+    let ctx = control.ctx;
+    let [width, height] = [ctx.canvas.width, ctx.canvas.height];
+    ctx.clearRect(0, 0, width, height);
+    for (let i = 0; i < tuple_len * numPoints; i += tuple_len) {
+      ctx.beginPath();
+      ctx.ellipse(
+        // x, y
+        coords[i], height - coords[i + 1],
+        // radiusX, radiusY
+        coords[i + 2], coords[i + 2],
+        // rotation
+        0,
+        // startAngle, endAngle
+        0, 2 * Math.PI);
+      ctx.stroke();
+    }
+    if (control.playEl.checked) {
+      requestAnimationFrame(control.renderLoop);
+    }
+  },
+};
+
+async function run() {
+  const wasm = await init();
+
+  // Configure canvas
+  const canvas = $('canvas');
+
+  control.init(wasm, canvas);
+}
+
+run();
---
+++ gassim/js/gassim.js
@@ -0,0 +1,483 @@
+
+let wasm;
+
+let cachegetInt32Memory = null;
+function getInt32Memory() {
+    if (cachegetInt32Memory === null || cachegetInt32Memory.buffer !== wasm.memory.buffer) {
+        cachegetInt32Memory = new Int32Array(wasm.memory.buffer);
+    }
+    return cachegetInt32Memory;
+}
+
+let cachegetFloat32Memory = null;
+function getFloat32Memory() {
+    if (cachegetFloat32Memory === null || cachegetFloat32Memory.buffer !== wasm.memory.buffer) {
+        cachegetFloat32Memory = new Float32Array(wasm.memory.buffer);
+    }
+    return cachegetFloat32Memory;
+}
+
+function getArrayF32FromWasm(ptr, len) {
+    return getFloat32Memory().subarray(ptr / 4, ptr / 4 + len);
+}
+
+let cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+
+let cachegetUint8Memory = null;
+function getUint8Memory() {
+    if (cachegetUint8Memory === null || cachegetUint8Memory.buffer !== wasm.memory.buffer) {
+        cachegetUint8Memory = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachegetUint8Memory;
+}
+
+function getStringFromWasm(ptr, len) {
+    return cachedTextDecoder.decode(getUint8Memory().subarray(ptr, ptr + len));
+}
+
+const heap = new Array(32);
+
+heap.fill(undefined);
+
+heap.push(undefined, null, true, false);
+
+let heap_next = heap.length;
+
+function addHeapObject(obj) {
+    if (heap_next === heap.length) heap.push(heap.length + 1);
+    const idx = heap_next;
+    heap_next = heap[idx];
+
+    heap[idx] = obj;
+    return idx;
+}
+
+function getObject(idx) { return heap[idx]; }
+
+let WASM_VECTOR_LEN = 0;
+
+let cachedTextEncoder = new TextEncoder('utf-8');
+
+const encodeString = (typeof cachedTextEncoder.encodeInto === 'function'
+    ? function (arg, view) {
+    return cachedTextEncoder.encodeInto(arg, view);
+}
+    : function (arg, view) {
+    const buf = cachedTextEncoder.encode(arg);
+    view.set(buf);
+    return {
+        read: arg.length,
+        written: buf.length
+    };
+});
+
+function passStringToWasm(arg) {
+
+    let len = arg.length;
+    let ptr = wasm.__wbindgen_malloc(len);
+
+    const mem = getUint8Memory();
+
+    let offset = 0;
+
+    for (; offset < len; offset++) {
+        const code = arg.charCodeAt(offset);
+        if (code > 0x7F) break;
+        mem[ptr + offset] = code;
+    }
+
+    if (offset !== len) {
+        if (offset !== 0) {
+            arg = arg.slice(offset);
+        }
+        ptr = wasm.__wbindgen_realloc(ptr, len, len = offset + arg.length * 3);
+        const view = getUint8Memory().subarray(ptr + offset, ptr + len);
+        const ret = encodeString(arg, view);
+
+        offset += ret.written;
+    }
+
+    WASM_VECTOR_LEN = offset;
+    return ptr;
+}
+
+function dropObject(idx) {
+    if (idx < 36) return;
+    heap[idx] = heap_next;
+    heap_next = idx;
+}
+
+function takeObject(idx) {
+    const ret = getObject(idx);
+    dropObject(idx);
+    return ret;
+}
+
+function handleError(e) {
+    wasm.__wbindgen_exn_store(addHeapObject(e));
+}
+
+function getArrayU8FromWasm(ptr, len) {
+    return getUint8Memory().subarray(ptr / 1, ptr / 1 + len);
+}
+
+function debugString(val) {
+    // primitive types
+    const type = typeof val;
+    if (type == 'number' || type == 'boolean' || val == null) {
+        return  `${val}`;
+    }
+    if (type == 'string') {
+        return `"${val}"`;
+    }
+    if (type == 'symbol') {
+        const description = val.description;
+        if (description == null) {
+            return 'Symbol';
+        } else {
+            return `Symbol(${description})`;
+        }
+    }
+    if (type == 'function') {
+        const name = val.name;
+        if (typeof name == 'string' && name.length > 0) {
+            return `Function(${name})`;
+        } else {
+            return 'Function';
+        }
+    }
+    // objects
+    if (Array.isArray(val)) {
+        const length = val.length;
+        let debug = '[';
+        if (length > 0) {
+            debug += debugString(val[0]);
+        }
+        for(let i = 1; i < length; i++) {
+            debug += ', ' + debugString(val[i]);
+        }
+        debug += ']';
+        return debug;
+    }
+    // Test for built-in
+    const builtInMatches = /\[object ([^\]]+)\]/.exec(toString.call(val));
+    let className;
+    if (builtInMatches.length > 1) {
+        className = builtInMatches[1];
+    } else {
+        // Failed to match the standard '[object ClassName]'
+        return toString.call(val);
+    }
+    if (className == 'Object') {
+        // we're a user defined class or Object
+        // JSON.stringify avoids problems with cycles, and is generally much
+        // easier than looping through ownProperties of `val`.
+        try {
+            return 'Object(' + JSON.stringify(val) + ')';
+        } catch (_) {
+            return 'Object';
+        }
+    }
+    // errors
+    if (val instanceof Error) {
+        return `${val.name}: ${val.message}\n${val.stack}`;
+    }
+    // TODO we could test for more things here, like `Set`s and `Map`s.
+    return className;
+}
+/**
+*/
+export class World {
+
+    static __wrap(ptr) {
+        const obj = Object.create(World.prototype);
+        obj.ptr = ptr;
+
+        return obj;
+    }
+
+    free() {
+        const ptr = this.ptr;
+        this.ptr = 0;
+
+        wasm.__wbg_world_free(ptr);
+    }
+    /**
+    * @returns {number}
+    */
+    get num_particles() {
+        const ret = wasm.__wbg_get_world_num_particles(this.ptr);
+        return ret >>> 0;
+    }
+    /**
+    * @param {number} arg0
+    */
+    set num_particles(arg0) {
+        wasm.__wbg_set_world_num_particles(this.ptr, arg0);
+    }
+    /**
+    * @param {number} width
+    * @param {number} height
+    * @param {number} gravity
+    * @param {number} num_particles
+    * @param {number} radius
+    * @param {number} mass
+    * @param {number} heat
+    * @param {number} van_der_waals
+    * @returns {World}
+    */
+    static new(width, height, gravity, num_particles, radius, mass, heat, van_der_waals) {
+        const ret = wasm.world_new(width, height, gravity, num_particles, radius, mass, heat, van_der_waals);
+        return World.__wrap(ret);
+    }
+    /**
+    * @param {number} x
+    * @param {number} y
+    * @param {number} radius
+    * @param {number} mass
+    * @param {number} heat
+    * @param {number} n
+    */
+    add_particles(x, y, radius, mass, heat, n) {
+        wasm.world_add_particles(this.ptr, x, y, radius, mass, heat, n);
+    }
+    /**
+    * @param {number} x
+    * @param {number} y
+    * @param {number} radius
+    */
+    remove_particles(x, y, radius) {
+        wasm.world_remove_particles(this.ptr, x, y, radius);
+    }
+    /**
+    * @returns {number}
+    */
+    update_positions() {
+        const ret = wasm.world_update_positions(this.ptr);
+        return ret;
+    }
+    /**
+    * @returns {number}
+    */
+    step() {
+        const ret = wasm.world_step(this.ptr);
+        return ret;
+    }
+    /**
+    * @param {number} x
+    * @param {number} y
+    * @param {number} radius
+    * @param {number} factor
+    */
+    temperature(x, y, radius, factor) {
+        wasm.world_temperature(this.ptr, x, y, radius, factor);
+    }
+    /**
+    * @returns {number}
+    */
+    timestep() {
+        const ret = wasm.world_timestep(this.ptr);
+        return ret;
+    }
+    /**
+    * @param {number} timestep
+    */
+    set_timestep(timestep) {
+        wasm.world_set_timestep(this.ptr, timestep);
+    }
+    /**
+    * @returns {Float32Array}
+    */
+    gravity() {
+        const retptr = 8;
+        const ret = wasm.world_gravity(retptr, this.ptr);
+        const memi32 = getInt32Memory();
+        const v0 = getArrayF32FromWasm(memi32[retptr / 4 + 0], memi32[retptr / 4 + 1]).slice();
+        wasm.__wbindgen_free(memi32[retptr / 4 + 0], memi32[retptr / 4 + 1] * 4);
+        return v0;
+    }
+    /**
+    * @param {number} gravity
+    */
+    set_gravity(gravity) {
+        wasm.world_set_gravity(this.ptr, gravity);
+    }
+}
+
+function init(module) {
+    if (typeof module === 'undefined') {
+        module = import.meta.url.replace(/\.js$/, '_bg.wasm');
+    }
+    let result;
+    const imports = {};
+    imports.wbg = {};
+    imports.wbg.__wbg_log_ea9df5fefa8f404d = function(arg0, arg1) {
+        console.log(getStringFromWasm(arg0, arg1));
+    };
+    imports.wbg.__wbg_new_59cb74e423758ede = function() {
+        const ret = new Error();
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbg_stack_558ba5917b466edd = function(arg0, arg1) {
+        const ret = getObject(arg1).stack;
+        const ret0 = passStringToWasm(ret);
+        const ret1 = WASM_VECTOR_LEN;
+        getInt32Memory()[arg0 / 4 + 0] = ret0;
+        getInt32Memory()[arg0 / 4 + 1] = ret1;
+    };
+    imports.wbg.__wbg_error_4bb6c2a97407129a = function(arg0, arg1) {
+        const v0 = getStringFromWasm(arg0, arg1).slice();
+        wasm.__wbindgen_free(arg0, arg1 * 1);
+        console.error(v0);
+    };
+    imports.wbg.__wbindgen_object_drop_ref = function(arg0) {
+        takeObject(arg0);
+    };
+    imports.wbg.__wbindgen_string_new = function(arg0, arg1) {
+        const ret = getStringFromWasm(arg0, arg1);
+        return addHeapObject(ret);
+    };
+    imports.wbg.__widl_f_now_Performance = function(arg0) {
+        const ret = getObject(arg0).now();
+        return ret;
+    };
+    imports.wbg.__wbg_newnoargs_8effd2c0e33a9e83 = function(arg0, arg1) {
+        const ret = new Function(getStringFromWasm(arg0, arg1));
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbg_call_11f5c018dea16986 = function(arg0, arg1) {
+        try {
+            const ret = getObject(arg0).call(getObject(arg1));
+            return addHeapObject(ret);
+        } catch (e) {
+            handleError(e)
+        }
+    };
+    imports.wbg.__wbg_globalThis_b8da724777cacbb6 = function() {
+        try {
+            const ret = globalThis.globalThis;
+            return addHeapObject(ret);
+        } catch (e) {
+            handleError(e)
+        }
+    };
+    imports.wbg.__wbg_self_78670bf6333531d2 = function() {
+        try {
+            const ret = self.self;
+            return addHeapObject(ret);
+        } catch (e) {
+            handleError(e)
+        }
+    };
+    imports.wbg.__wbg_window_b19864ecbde8d123 = function() {
+        try {
+            const ret = window.window;
+            return addHeapObject(ret);
+        } catch (e) {
+            handleError(e)
+        }
+    };
+    imports.wbg.__wbg_global_c6db5ff079ba98ed = function() {
+        try {
+            const ret = global.global;
+            return addHeapObject(ret);
+        } catch (e) {
+            handleError(e)
+        }
+    };
+    imports.wbg.__wbindgen_is_undefined = function(arg0) {
+        const ret = getObject(arg0) === undefined;
+        return ret;
+    };
+    imports.wbg.__wbindgen_object_clone_ref = function(arg0) {
+        const ret = getObject(arg0);
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbg_get_a7395f826d52f42a = function(arg0, arg1) {
+        try {
+            const ret = Reflect.get(getObject(arg0), getObject(arg1));
+            return addHeapObject(ret);
+        } catch (e) {
+            handleError(e)
+        }
+    };
+    imports.wbg.__wbg_self_1b7a39e3a92c949c = function() {
+        try {
+            const ret = self.self;
+            return addHeapObject(ret);
+        } catch (e) {
+            handleError(e)
+        }
+    };
+    imports.wbg.__wbg_require_604837428532a733 = function(arg0, arg1) {
+        const ret = require(getStringFromWasm(arg0, arg1));
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbg_crypto_968f1772287e2df0 = function(arg0) {
+        const ret = getObject(arg0).crypto;
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbg_getRandomValues_a3d34b4fee3c2869 = function(arg0) {
+        const ret = getObject(arg0).getRandomValues;
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbg_getRandomValues_f5e14ab7ac8e995d = function(arg0, arg1, arg2) {
+        getObject(arg0).getRandomValues(getArrayU8FromWasm(arg1, arg2));
+    };
+    imports.wbg.__wbg_randomFillSync_d5bd2d655fdf256a = function(arg0, arg1, arg2) {
+        getObject(arg0).randomFillSync(getArrayU8FromWasm(arg1, arg2));
+    };
+    imports.wbg.__wbindgen_debug_string = function(arg0, arg1) {
+        const ret = debugString(getObject(arg1));
+        const ret0 = passStringToWasm(ret);
+        const ret1 = WASM_VECTOR_LEN;
+        getInt32Memory()[arg0 / 4 + 0] = ret0;
+        getInt32Memory()[arg0 / 4 + 1] = ret1;
+    };
+    imports.wbg.__wbindgen_throw = function(arg0, arg1) {
+        throw new Error(getStringFromWasm(arg0, arg1));
+    };
+
+    if ((typeof URL === 'function' && module instanceof URL) || typeof module === 'string' || (typeof Request === 'function' && module instanceof Request)) {
+
+        const response = fetch(module);
+        if (typeof WebAssembly.instantiateStreaming === 'function') {
+            result = WebAssembly.instantiateStreaming(response, imports)
+            .catch(e => {
+                return response
+                .then(r => {
+                    if (r.headers.get('Content-Type') != 'application/wasm') {
+                        console.warn("`WebAssembly.instantiateStreaming` failed because your server does not serve wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n", e);
+                        return r.arrayBuffer();
+                    } else {
+                        throw e;
+                    }
+                })
+                .then(bytes => WebAssembly.instantiate(bytes, imports));
+            });
+        } else {
+            result = response
+            .then(r => r.arrayBuffer())
+            .then(bytes => WebAssembly.instantiate(bytes, imports));
+        }
+    } else {
+
+        result = WebAssembly.instantiate(module, imports)
+        .then(result => {
+            if (result instanceof WebAssembly.Instance) {
+                return { instance: result, module };
+            } else {
+                return result;
+            }
+        });
+    }
+    return result.then(({instance, module}) => {
+        wasm = instance.exports;
+        init.__wbindgen_wasm_module = module;
+
+        return wasm;
+    });
+}
+
+export default init;
+
